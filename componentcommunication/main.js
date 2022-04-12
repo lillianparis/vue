5e180edce1c57b4f769b8ef8d4f478da3af70aae
Lilli Paris <lillian.paris@simpleviewinc.com>
2022-04-12 16:47:57 -0700

    Learning .

---
+++ componentcommunication/main.js
@@ -0,0 +1,21 @@
+Vue.component('coupon', {
+    template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
+
+    methods: {
+        onCouponApplied() {
+           this.$emit('applied')
+        }
+    }
+});
+
+// This is the root instance (parent)
+new Vue ({
+    el: '#root',
+    methods: {
+        onCouponApplied() {
+            alert('it was applied');
+        }
+
+    }
+})
+
